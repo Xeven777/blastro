693b056b9d5c2e6ae2c44c95baaa5f84ecd426d1
Anish <xevenbiswas@gmail.com>
2025-01-06 22:57:10 +0530

    Add Google Analytics and Clarity components; update package.json for Partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import partytown from "@astrojs/partytown";
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [mdx(), sitemap(), tailwind(), partytown()],
 });
